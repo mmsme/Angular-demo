7e8b5062fc17af413630dd72c7458274a389124d
mmsme <mohamed@DESKTOP-FGHL2VH.localdomain>
2021-02-05 16:58:08 +0200

    ready

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/dist"));
+
+app.all("*", (res, req) => {
+  res.statusCode(200).sendFile(__dirname + "/dist/index.html");
+});
+
+app.listen(process.env.PORT || 8000);
